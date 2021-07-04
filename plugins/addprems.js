2510e86c6ecb6fa26415afccaa760f27df7d2a64
KioniNdungu <84273819+KioniNdungu@users.noreply.github.com>
2021-07-04 10:57:28 +0300

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (global.prems.includes(who.split`@`[0])) throw 'Sudah premium!'
+    if (global.prems.includes(who.split`@`[0])) throw 'Already premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium!`, m, {
+    conn.reply(m.chat, `Hi, @${who.split`@`[0]}. You are already using premium!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
