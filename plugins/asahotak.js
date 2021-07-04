561150893a3c6bad4baa1b85b1eebe52b8ce5f7a
KioniNdungu <84273819+KioniNdungu@users.noreply.github.com>
2021-07-04 11:22:04 +0300

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
     if (id in conn.asahotak) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.asahotak[id][0])
         throw false
     }
     let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
@@ -17,20 +17,20 @@ let handler = async (m, { conn, usedPrefix }) => {
 ${json.result.soal}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}ao untuk bantuan
+type ${usedPrefix}ao for help
 Bonus: ${poin} XP
 `.trim()
     conn.asahotak[id] = [
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.asahotak[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.asahotak[id][0])
+            if (conn.asahotak[id]) conn.reply(m.chat, `Time is up!\nThe answer is *${json.result.jawaban}*`, conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
 }
-handler.help = ['asahotak']
+handler.help = ['brainteaser']
 handler.tags = ['game']
-handler.command = /^asahotak/i
+handler.command = /^brainteaser/i
 
 module.exports = handler
