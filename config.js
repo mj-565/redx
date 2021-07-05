3f1f73f0a843bc67508493c413f33cfae088abcc
mj-565 <80670736+mj-565@users.noreply.github.com>
2021-07-05 14:07:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['+1 (613) 902-7774','254715369906'] // Put your number here
+global.owner = ['+1 (613) 902-7774','+254715369906'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
